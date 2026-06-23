9795e6d7801f540b54aac79a3fc9acac4d2246d0
Hiroki Kawakami <40638139+Hiroki-Kawakami@users.noreply.github.com>
2026-06-24 05:08:27 +0900

    HomeScreen Layout

--- app/screens/HomeScreen.cpp
+++ app/screens/HomeScreen.cpp
@@ -3,7 +3,7 @@
 
 void HomeScreen::build() {
     lv_obj_set_flex_flow(root_, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_flex_align(root_, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    lv_obj_set_flex_align(root_, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
     lv_obj_set_style_pad_all(root_, 20, 0);
 
     { // Title
@@ -17,6 +17,60 @@ void HomeScreen::build() {
 
         lv_obj_set_style_pad_top(title, 40, 0);
         lv_obj_set_style_pad_bottom(version, 40, 0);
+    }
+
+    { // My Card
+        auto button = lv_button_create(root_);
+        lv_obj_set_size(button, LV_PCT(100), 320);
+        lv_obj_set_flex_flow(button, LV_FLEX_FLOW_ROW);
+        lv_obj_set_flex_align(button, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+        auto image = lv_image_create(button);
+        lv_obj_set_size(image, 169, 300);
+        lv_obj_set_style_bg_color(image, lv_color_hex(0x888888), 0);
+        lv_obj_set_style_bg_opa(image, LV_OPA_COVER, 0);
+
+        auto container = lv_container_create(button, LV_FLEX_FLOW_COLUMN);
+        lv_obj_remove_flag(container, LV_OBJ_FLAG_CLICKABLE);
+        lv_obj_set_size(container, 304, 300);
+        lv_obj_set_style_pad_row(container, 10, 0);
+
+        auto title = lv_label_create(container);
+        lv_label_set_text(title, "My Card");
+        lv_obj_set_style_text_font(title, &lv_font_montserrat_32, 0);
+        lv_obj_set_style_pad_top(title, 20, 0);
+        lv_spacer_create(container, 0, 0, 1);
+
+        auto name = lv_label_create(container);
+        lv_label_set_text(name, "Hiroki Kawakami");
+        lv_obj_set_style_text_font(name, &lv_font_montserrat_32, 0);
+
+        auto id = lv_label_create(container);
+        lv_label_set_text(id, "@hiroki_cockatoo");
+        lv_obj_set_style_text_font(id, &lv_font_montserrat_24, 0);
+
+        lv_spacer_create(container, 0, 0, 1);
+
+        lv_hor_separator_create(container, 20);
+        auto share_button = lv_button_create(container);
+        lv_obj_remove_style_all(share_button);
+        lv_obj_set_size(share_button, LV_PCT(100), 80);
+        lv_obj_set_style_margin_hor(share_button, 20, 0);
+        lv_obj_set_flex_flow(share_button, LV_FLEX_FLOW_ROW);
+        lv_obj_set_flex_align(share_button, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+        lv_obj_set_style_pad_column(share_button, 10, 0);
+        lv_obj_set_style_border_width(share_button, 2, 0);
+        lv_obj_set_style_radius(share_button, 8, 0);
+        lv_obj_set_style_border_color(share_button, lv_color_white(), 0);
+        lv_obj_set_style_border_color(share_button, lv_color_black(), LV_STATE_PRESSED);
+
+        auto share_icon = lv_label_create(share_button);
+        lv_label_set_text(share_icon, LUCIDE_SQUARE_ARROW_OUT_UP_RIGHT);
+        lv_obj_set_style_text_font(share_icon, R.font.lucide_40, 0);
+        auto share_label = lv_label_create(share_button);
+        lv_label_set_text(share_label, "Share");
+        lv_obj_set_style_text_font(share_label, &lv_font_montserrat_32, 0);
+
     }
 
     { // Buttons
